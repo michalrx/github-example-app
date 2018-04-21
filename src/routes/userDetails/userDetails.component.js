86c0acef933367e0c924c3c5cba6577adcca98d3
Michal Roguski <michalrx@gmail.com>
2018-04-21 13:57:34 +0200

    Add user details

---
+++ src/routes/userDetails/userDetails.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container } from "./userDetails.style";
+
+class UserDetails extends Component {
+
+    render() {
+        return (
+            <Container>
+
+            </Container>
+        );
+    }
+}
+
+export default UserDetails;
---
+++ src/routes/userDetails/userDetails.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { colors } from "../../utils/styles/color.style";
+
+export const Container = styled.div`
+
+`;
