85098c878c76943127cff048079dd5c8dff40bc5
Michal Roguski <michalrx@gmail.com>
2018-04-21 12:32:51 +0200

    Add user to state

--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import { Container } from "./home.style";
+import { Container, UserLoginBox, UserInput, UserButton } from "./home.style";
+import { Wrapper } from "../../utils/styles/global.style";
+
 import Header from "../../components/header/header.component";
 import Footer from "../../components/footer/footer.component";
 
 class Home extends Component {
+    state = {
+        users: [],
+        inputValue: '',
+    };
+
+    addNewUser = () => {
+        this.setState({
+            users: [...this.state.users, this.state.inputValue],
+            inputValue: '',
+            });
+    };
+
+    userOnChange = (e) => {
+        this.setState({inputValue: e.target.value});
+    };
+
     render() {
         return (
             <Container>
                 <Header/>
-                home
+
+                <Wrapper>
+                    <UserLoginBox>
+                        <div>Please add GitHub username to observable.</div>
+
+                            <UserInput value={this.state.inputValue} onChange={this.userOnChange} />
+                            <UserButton onClick={this.addNewUser}>
+                                Add user
+                            </UserButton>
+
+                    </UserLoginBox>
+                </Wrapper>
+
                 <Footer/>
             </Container>
         );
--- src/routes/home/home.style.js
+++ src/routes/home/home.style.js
@@ -1,5 +1,35 @@
 import styled from 'styled-components';
+import { colors } from "../../utils/styles/color.style";
 
 export const Container = styled.div`
 
+`;
+
+export const UserLoginBox = styled.div`
+    color: ${colors.dark};
+    border: 1px solid ${colors.dark};
+    border-radius: 2.5px;
+    margin: 50px auto;
+    width: 500px;
+    text-align: center;
+    padding: 20px;
+`;
+
+export const UserInput = styled.input`
+    border: 1px solid ${colors.light};
+    margin-top: 15px;
+    padding: 5px;
+    
+`;
+
+export const UserButton = styled.button`
+    background: ${colors.dark};
+    color: ${colors.white};
+    border: 0;
+    margin-top: 15px;
+    padding: 6px;
+    
+    &:hover {
+    background: ${colors.medium};
+    }
 `;
